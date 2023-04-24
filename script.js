40139df823bb20666626cf6c3ebc0342b80594d6
Gonoko35 <gonok17@hotmail.com>
2023-04-24 15:34:01 -0300

    Algo de JS

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 
 
-let descrip= document.getElementById("descripcion");
-console.log(descrip.innerText);
+
+
+
+function cambiar_parrafo(){
+    
+    document.getElementById("descripcion-edit").style.display="block"; 
+    let texto= document.getElementById("descripcion").innerText;
+}
+
+function function2(valor){
+          
+    document.getElementById("descripcion").innerText=valor;
+}
+
+
+let textarea=document.getElementById("descripcion-edit")
+textarea.addEventListener('keyup', (e) => {
+    
+    if (e.key=="Enter") {
+        document.getElementById("descripcion-edit").style.display="none";
+    }
+
+} )
